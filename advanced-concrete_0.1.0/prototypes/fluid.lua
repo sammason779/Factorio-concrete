b8bd0309335030add207621783728bb9241e8736
sammason779 <72918680+sammason779@users.noreply.github.com>
2022-04-03 00:31:55 +0100

    Update fluid.lua

--- advanced-concrete_0.1.0/prototypes/fluid.lua
+++ advanced-concrete_0.1.0/prototypes/fluid.lua
@@ -6,7 +6,7 @@ data:extend({
     default_temperature = 10,
     max_temperature = 35,
     heat_capacity = "0.2KJ",
-	base_color = {r=0.5, g=0.5, b=0.5},
+    base_color = {r=0.5, g=0.5, b=0.5},
     flow_color = {r=1.0, g=1.0, b=1.0},
     icon = "__advanced-concrete__/graphics/fluid/cement.png",
     icon_size = 64, icon_mipmaps = 4,
